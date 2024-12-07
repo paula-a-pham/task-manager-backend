10ba7da11ba44cf607b5bada39262f28f3464392
Paula Ashraf <paula.a.pham@gmail.com>
2024-12-07 22:14:18 +0200

    create basic models

---
+++ Data/Models/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Board
+    {
+        public byte Id { get; set; }    
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Task>? Tasks { get; set; }
+    }
+}
---
+++ Data/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? LastUpdate { get; set;}
+        public byte Priority { get; set; }
+        public ICollection<string>? Tags { get; set; }
+        public byte BoardId { get; set; }
+        public required Board Board { get; set; }    
+
+    }
+}
