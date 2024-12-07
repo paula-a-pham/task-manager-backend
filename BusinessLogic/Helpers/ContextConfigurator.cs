ee113bdb11540ccbcc3a1a4cfcea76eb618c001b
Paula Ashraf <paula.a.pham@gmail.com>
2024-12-07 22:45:52 +0200

    create extension method to configure db context in presentation layer

---
+++ BusinessLogic/Helpers/ContextConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogic.Helpers;
+
+public static class ContextConfigurator
+{
+    public static void ApplicationContextConfigurator(this IServiceCollection services, string? connectionString)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+    }
+}
